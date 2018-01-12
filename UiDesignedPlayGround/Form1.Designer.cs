274c09bed0b654efdb2ae046e2a22318a9dcf10a
MissingPotato <potatomissing@gmail.com>
2018-01-12 02:49:52 +0200

    Initial commit

---
+++ UiDesignedPlayGround/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace UiDesignedPlayGround
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.panel_toolBar = new System.Windows.Forms.Panel();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.checkBox_ImagePlay = new System.Windows.Forms.CheckBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.checkBox_Home = new System.Windows.Forms.CheckBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.Title = new System.Windows.Forms.Label();
+			this.panel_Context = new System.Windows.Forms.Panel();
+			this.button_Close = new System.Windows.Forms.Button();
+			this.button_Minimize = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.asd = new System.Windows.Forms.Label();
+			this.panel_toolBar.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel_Context.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel_toolBar
+			// 
+			this.panel_toolBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+			this.panel_toolBar.Controls.Add(this.pictureBox3);
+			this.panel_toolBar.Controls.Add(this.checkBox_ImagePlay);
+			this.panel_toolBar.Controls.Add(this.pictureBox2);
+			this.panel_toolBar.Controls.Add(this.checkBox_Home);
+			this.panel_toolBar.Controls.Add(this.panel1);
+			this.panel_toolBar.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel_toolBar.ForeColor = System.Drawing.SystemColors.Control;
+			this.panel_toolBar.Location = new System.Drawing.Point(0, 0);
+			this.panel_toolBar.Name = "panel_toolBar";
+			this.panel_toolBar.Size = new System.Drawing.Size(211, 568);
+			this.panel_toolBar.TabIndex = 0;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(3, 96);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(35, 31);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 6;
+			this.pictureBox3.TabStop = false;
+			// 
+			// checkBox_ImagePlay
+			// 
+			this.checkBox_ImagePlay.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox_ImagePlay.Dock = System.Windows.Forms.DockStyle.Top;
+			this.checkBox_ImagePlay.FlatAppearance.BorderSize = 0;
+			this.checkBox_ImagePlay.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+			this.checkBox_ImagePlay.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+			this.checkBox_ImagePlay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+			this.checkBox_ImagePlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.checkBox_ImagePlay.Font = new System.Drawing.Font("Roboto Light", 15F);
+			this.checkBox_ImagePlay.ForeColor = System.Drawing.SystemColors.Control;
+			this.checkBox_ImagePlay.Location = new System.Drawing.Point(0, 91);
+			this.checkBox_ImagePlay.Name = "checkBox_ImagePlay";
+			this.checkBox_ImagePlay.Size = new System.Drawing.Size(211, 41);
+			this.checkBox_ImagePlay.TabIndex = 5;
+			this.checkBox_ImagePlay.Text = "        ImagePlay";
+			this.checkBox_ImagePlay.UseVisualStyleBackColor = true;
+			this.checkBox_ImagePlay.CheckedChanged += new System.EventHandler(this.checkBox_ImagePlay_CheckedChanged);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(3, 56);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(35, 31);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 4;
+			this.pictureBox2.TabStop = false;
+			// 
+			// checkBox_Home
+			// 
+			this.checkBox_Home.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox_Home.Dock = System.Windows.Forms.DockStyle.Top;
+			this.checkBox_Home.FlatAppearance.BorderSize = 0;
+			this.checkBox_Home.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+			this.checkBox_Home.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+			this.checkBox_Home.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+			this.checkBox_Home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.checkBox_Home.Font = new System.Drawing.Font("Roboto Light", 15F);
+			this.checkBox_Home.ForeColor = System.Drawing.SystemColors.Control;
+			this.checkBox_Home.Location = new System.Drawing.Point(0, 50);
+			this.checkBox_Home.Name = "checkBox_Home";
+			this.checkBox_Home.Size = new System.Drawing.Size(211, 41);
+			this.checkBox_Home.TabIndex = 3;
+			this.checkBox_Home.Text = "        Home";
+			this.checkBox_Home.UseVisualStyleBackColor = true;
+			this.checkBox_Home.CheckedChanged += new System.EventHandler(this.checkBox_Home_CheckedChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(76)))));
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.Title);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(211, 50);
+			this.panel1.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(3, 5);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Title
+			// 
+			this.Title.AutoSize = true;
+			this.Title.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold);
+			this.Title.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.Title.Location = new System.Drawing.Point(44, 11);
+			this.Title.Name = "Title";
+			this.Title.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.Title.Size = new System.Drawing.Size(167, 29);
+			this.Title.TabIndex = 0;
+			this.Title.Text = "PLAYGROUND";
+			// 
+			// panel_Context
+			// 
+			this.panel_Context.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.panel_Context.Controls.Add(this.asd);
+			this.panel_Context.Controls.Add(this.panel2);
+			this.panel_Context.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel_Context.ForeColor = System.Drawing.SystemColors.Control;
+			this.panel_Context.Location = new System.Drawing.Point(211, 0);
+			this.panel_Context.Name = "panel_Context";
+			this.panel_Context.Size = new System.Drawing.Size(773, 568);
+			this.panel_Context.TabIndex = 1;
+			// 
+			// button_Close
+			// 
+			this.button_Close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_Close.BackgroundImage")));
+			this.button_Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button_Close.FlatAppearance.BorderSize = 0;
+			this.button_Close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(76)))));
+			this.button_Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(207)))), ((int)(((byte)(146)))));
+			this.button_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_Close.Location = new System.Drawing.Point(722, 5);
+			this.button_Close.Name = "button_Close";
+			this.button_Close.Size = new System.Drawing.Size(42, 41);
+			this.button_Close.TabIndex = 0;
+			this.button_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this.button_Close.UseVisualStyleBackColor = true;
+			this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+			this.button_Close.Paint += new System.Windows.Forms.PaintEventHandler(this.button_Close_Paint);
+			// 
+			// button_Minimize
+			// 
+			this.button_Minimize.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_Minimize.BackgroundImage")));
+			this.button_Minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button_Minimize.FlatAppearance.BorderSize = 0;
+			this.button_Minimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(76)))));
+			this.button_Minimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(207)))), ((int)(((byte)(146)))));
+			this.button_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_Minimize.Location = new System.Drawing.Point(674, 5);
+			this.button_Minimize.Name = "button_Minimize";
+			this.button_Minimize.Size = new System.Drawing.Size(42, 41);
+			this.button_Minimize.TabIndex = 1;
+			this.button_Minimize.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this.button_Minimize.UseVisualStyleBackColor = true;
+			this.button_Minimize.Click += new System.EventHandler(this.button_Minimize_Click);
+			this.button_Minimize.Paint += new System.Windows.Forms.PaintEventHandler(this.button_Minimize_Paint);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Roboto Thin", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+			this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label1.Location = new System.Drawing.Point(6, 11);
+			this.label1.Name = "label1";
+			this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.label1.Size = new System.Drawing.Size(127, 29);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Dashboard";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.button_Minimize);
+			this.panel2.Controls.Add(this.button_Close);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(773, 50);
+			this.panel2.TabIndex = 0;
+			this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+			this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+			this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+			// 
+			// asd
+			// 
+			this.asd.AutoSize = true;
+			this.asd.Font = new System.Drawing.Font("Roboto Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.asd.ForeColor = System.Drawing.Color.DarkRed;
+			this.asd.Location = new System.Drawing.Point(-77, 68);
+			this.asd.Name = "asd";
+			this.asd.Size = new System.Drawing.Size(542, 38);
+			this.asd.TabIndex = 1;
+			this.asd.Text = "<---- MAKE EM BUTTONS, FUCK IT\\";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(984, 568);
+			this.Controls.Add(this.panel_Context);
+			this.Controls.Add(this.panel_toolBar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "PlayGround";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			this.panel_toolBar.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel_Context.ResumeLayout(false);
+			this.panel_Context.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel_toolBar;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label Title;
+		private System.Windows.Forms.CheckBox checkBox_Home;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.CheckBox checkBox_ImagePlay;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panel_Context;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button_Minimize;
+		private System.Windows.Forms.Button button_Close;
+		private System.Windows.Forms.Label asd;
+	}
+}
+
---
+++ UiDesignedPlayGround/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UiDesignedPlayGround
+{
+	public partial class Form1 : Form
+	{
+
+		#region Rounding
+
+		[System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+		private static extern IntPtr CreateRoundRectRgn
+		  (
+		   int nLeftRect, // x-coordinate of upper-left corner
+		   int nTopRect, // y-coordinate of upper-left corner
+		   int nRightRect, // x-coordinate of lower-right corner
+		   int nBottomRect, // y-coordinate of lower-right corner
+		   int nWidthEllipse, // height of ellipse
+		   int nHeightEllipse // width of ellipse
+		  );
+
+		[System.Runtime.InteropServices.DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+		private static extern bool DeleteObject(System.IntPtr hObject);
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			System.IntPtr ptr = CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
+			this.Region = System.Drawing.Region.FromHrgn(ptr);
+			DeleteObject(ptr);
+		}
+
+		private void button_Close_Paint(object sender, PaintEventArgs e)
+		{
+			IntPtr ptr = CreateRoundRectRgn(0, 0, button_Close.Width, button_Close.Height, 10, 10);
+			button_Close.Region = Region.FromHrgn(ptr);
+			DeleteObject(ptr);
+		}
+
+		private void button_Minimize_Paint(object sender, PaintEventArgs e)
+		{
+			IntPtr ptr = CreateRoundRectRgn(0, 0, button_Minimize.Width, button_Minimize.Height, 10, 10);
+			button_Minimize.Region = Region.FromHrgn(ptr);
+			DeleteObject(ptr);
+		}
+
+		#endregion
+
+		public List<CheckBox> checkBoxList = new List<CheckBox>();
+		public Point lastPoint;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			checkBoxList.Add(checkBox_Home);
+			checkBoxList.Add(checkBox_ImagePlay);
+		}
+
+		#region WindowDragging
+		private void panel2_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+		private void panel2_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.Left += e.X - lastPoint.X;
+				this.Top += e.Y - lastPoint.Y;
+			}
+		}
+		private void panel2_MouseDown(object sender, MouseEventArgs e)
+		{
+			lastPoint = new Point(e.X, e.Y);
+		}
+		#endregion
+
+		#region Controller Buttons
+		private void button_Close_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+		private void button_Minimize_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+		}
+		#endregion
+
+		#region CheckBoxes
+
+		private void checkBox_Home_CheckedChanged(object sender, EventArgs e)
+		{
+			ResetCheckBoxes((CheckBox)sender);
+		}
+		private void checkBox_ImagePlay_CheckedChanged(object sender, EventArgs e)
+		{
+			ResetCheckBoxes((CheckBox)sender);
+		}
+		
+		#endregion
+		
+		#region DynamicScripts
+
+		public void ResetCheckBoxes(CheckBox sender)
+		{
+			foreach (CheckBox box in checkBoxList)
+			{
+				if (!box.Equals (sender) )
+					box.Checked = false;
+				else
+					box.CheckState = box.CheckState;
+
+				
+
+			}
+		}
+
+
+		#endregion
+
+		
+	}
+}
